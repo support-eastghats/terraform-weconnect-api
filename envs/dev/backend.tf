fb85d6c6c10105a965c6be10eb4734076995f92f
Pavithran Ambur Venkatesan <support@eastghats.com>
2025-04-16 14:55:03 +0530

    Add backend config for S3 remote state

---
+++ envs/dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "weconnect-terraform-state-dev"     # your backend bucket name
+    key            = "dev/terraform.tfstate"              # path inside the bucket
+    region         = "ap-south-1"
+    encrypt        = true
+  }
+}
